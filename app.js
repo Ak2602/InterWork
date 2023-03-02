bb45c8560621cf8642ff309728160da9f20b58c3
Ak2602 <anishkhedekar35@gmail.com>
2023-03-02 19:35:38 +0530

    Updated: added Login section..

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import bodyParser from 'body-parser'; // to collect the data from user
 import flash from "express-flash"; //to show the popup message
 import { fileURLToPath } from 'url';
 import con from './database.js';
+import { name } from 'ejs';
 
 
 //import indexRouter from './routes/index';
@@ -27,7 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
@@ -35,38 +36,73 @@ app.use(session({
   secret: "sukuna26",
   resave: false,
   saveUninitialized: true,
-  Cookie: {maxAge:60000}
+  Cookie: { maxAge: 60000 }
 }));
 
 app.use(flash());
 
-app.get('/', (req, res)=>{
-  res.render('index', {title:'Register'});
+app.get('/', (req, res) => {
+  res.render('index', { title: 'WELCOME' });
 });
 
-app.post('/Auth', (function(req, res, next){
+app.get('/register', (req, res) => {
+  res.render('register', { title: 'register' });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', { title: 'login' });
+});
+
+app.post('/save', (function (req, res, next) {
   let user = req.body.username;
   let pass = req.body.password;
 
   let qry = (`INSERT INTO details (name, password) VALUES ("${user}", "${pass}")`);
-  con.query(qry, function(err, result){
-    if (err){
+  con.query(qry, function (err, result) {
+    if (err) {
       console.log("User Not Registered");
-    }else{
+    } else {
       console.log("record Updated");
       req.flash('success', 'User Added Successfully');
-      res.redirect('/');
+      res.redirect('/register');
     }
   });
 }));
 
+app.post('/auth', (function (req, res, next) {
+  let us = req.body.client;
+  let ps = req.body.secure;
+  if (us && ps) {
+    let compareQry = (`SELECT * FROM details WHERE name = "${us}" AND password = ${ps}`);
+    con.query(compareQry, function (err, rows, fields) {
+      if (err) throw err; 
+      if(rows.length<= 0){
+        console.log("false");
+        res.redirect("/");
+      }else {
+        console.log("true");
+        req.session.loggedin = true;
+        req.session.name = name;
+        req.flash('success', 'Login Successfull')
+        res.redirect('/login');
+        // console.log("record Updated");
+        // req.flash('success', 'User Added Successfully');
+        // res.redirect('/');
+        
+      }
+    });
+  }else{
+    res.send("Enter Credentials !!!");
+  }
+}));
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -77,7 +113,7 @@ app.use(function(err, req, res, next) {
 });
 
 // listining to port
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server is Running at ${port} `);
 });
 //export the app
