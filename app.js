6c05b7478d19773d46b44fcf4f2678baf58ad62b
Ak2602 <anishkhedekar35@gmail.com>
2023-03-07 09:01:10 +0530

    Added defferent Routes and Controllers

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ import session, { Cookie } from 'express-session'; // used to keep track about u
 import bodyParser from 'body-parser'; // to collect the data from user
 import flash from "express-flash"; //to show the popup message
 import { fileURLToPath } from 'url';
-import con from './database.js';
-import { name } from 'ejs';
 
+import router from './src/routes/Router.js';
 
-//import indexRouter from './routes/index';
-// import usersRouter from './routes/users';
 
 const app = express();
 const port = process.env.port || 3000;
@@ -23,15 +20,12 @@ const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 app.use(session({
   secret: "sukuna26",
   resave: false,
@@ -41,93 +35,11 @@ app.use(session({
 
 app.use(flash());
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'WELCOME' });
-});
+/* USING ROUTERS*/
 
-app.get('/register', (req, res) => {
-  res.render('register', { title: 'register' });
-});
-
-app.get('/login', (req, res) => {
-  res.render('login', { title: 'login' });
-});
-
-app.get('/admin', (req, res) => {
-  res.render('admin', { title: 'admin' });
-});
-
-app.post('/save', (function (req, res, next) {
-  let user = req.body.username;
-  let pass = req.body.password;
-  let searchQry = (`SELECT * FROM details WHERE name = "${user}"`);
-  let qry = (`INSERT INTO details (name, password) VALUES ("${user}", "${pass}")`);
-  con.query(searchQry, function (err, result) {
-    if (err) {
-      req.flash('warn', 'Failed to register!!');
-      res.redirect('/register');
-    }
-    if (result.length !== 0) {
-      req.flash('warn', 'user already exists!!');
-      res.redirect('/register');
-
-    } else {
-      con.query(qry, function (err, results) {
-        if (err) throw err;
-        console.log("Registered Successfully!!");
-        req.flash('success', 'User Added Successfully');
-        res.redirect('/register');
-      });
-    }
-  });
-}));
-
-app.post('/auth', (function (req, res, next) {
-  let us = req.body.client;
-  let ps = req.body.secure;
-  if (us && ps) {
-    let compareQry = (`SELECT * FROM details WHERE name = "${us}" AND password = ${ps}`);
-    con.query(compareQry, function (err, rows, fields) {
-      if (err) throw err;
-      if (rows.length <= 0) {
-        req.flash('warn', 'Invalid Credentials!!!')
-        res.redirect("/login");
-      } else {
-        console.log("true");
-        req.session.loggedin = true;
-        req.session.name = name;
-        req.flash('success', 'Login Successfull');
-        res.redirect('/admin');
-      }
-    });
-  } else {
-    req.flash('warn', 'Enter Details');
-  }
-}));
-
-app.post('/update', function (req, res) {
-  let n_ame = req.body.n_ame;
-  let p_ass = req.body.p_ass;
-  let sQry = (`SELECT * FROM details WHERE name = "${n_ame}"`);
-  let updateQuery = (`UPDATE details SET password = ${p_ass} WHERE name = "${n_ame}" `);
-  con.query(sQry, function (err, result) {
-    if (err) {
-      req.flash('warn', 'Failed to update!!');
-      res.redirect('/admin');
-    }
-    if (result.length == 0) {
-      req.flash('warn', 'record does not exists!!');
-      res.redirect('/admin');
-    }else{
-      con.query(updateQuery, function(err, results){
-        if (err) throw err;
-        console.log("true");
-        req.flash('success', 'Record updated Successfully');
-        res.redirect('/admin'); 
-      });
-    }
-  });
-});
+app.use('/', router);
+app.use('/register', router);
+app.use('login', router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
